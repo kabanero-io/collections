7606c6cb8ecf149996ddefed03289fe3a0a1acd3
kilnerm@uk.ibm.com <kilnerm@uk.ibm.com>
2020-02-06 11:42:40 +0000

    try sudo

--- ci/install_yq.sh
+++ ci/install_yq.sh
@@ -2,7 +2,7 @@
 
 cd /tmp
 wget https://github.com/mikefarah/yq/releases/download/3.0.1/yq_linux_amd64 
-cp ./yq_linux_amd64 /usr/local/bin/yq
-chmod 755 /usr/local/bin/yq
+sudo cp ./yq_linux_amd64 /usr/local/bin/yq
+sudo chmod 755 /usr/local/bin/yq
 
 yq --version
