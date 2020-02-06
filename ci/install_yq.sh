e9a5868e977d7fb40a58c75a92d8505933962e68
kilnerm@uk.ibm.com <kilnerm@uk.ibm.com>
2020-02-06 11:47:05 +0000

    use correct yq level

--- ci/install_yq.sh
+++ ci/install_yq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /tmp
-wget https://github.com/mikefarah/yq/releases/download/3.0.1/yq_linux_amd64 
+wget https://github.com/mikefarah/yq/releases/download/2.4.1/yq_linux_amd64
 sudo cp ./yq_linux_amd64 /usr/local/bin/yq
 sudo chmod 755 /usr/local/bin/yq
 
